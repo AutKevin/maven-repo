27df1c158112b96a18757bf6195e9aff78ed6fe9
AutKevin <qy814379857@foxmail.com>
2020-03-09 17:54:37 +0800

    下载文件到本地,本地文件转码

--- src/main/java/com/autumn/tool/FileUtil.java
+++ src/main/java/com/autumn/tool/FileUtil.java
@@ -5,8 +5,9 @@ import org.apache.commons.io.FilenameUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * @program: FileUtil
@@ -28,8 +29,7 @@ public class FileUtil {
 
     /**
      * 创建文件
-     *
-     * @param filePath
+     * @param filePath 文件全路径
      * @return
      */
     public static File createFile(String filePath) {
@@ -46,5 +46,93 @@ public class FileUtil {
             //e.printStackTrace();
         }
         return file;
+    }
+
+    /**
+     * 下载url的文件到指定文件路径里面,如果文件父文件夹不存在则自动创建
+     * url 下载的http地址
+     * path 文件存储地址+文件名
+     * return 如果文件大小大于2k则返回true
+     */
+    public static boolean downloadCreateDir(String url,String path){
+        HttpURLConnection connection;
+        connection = null;
+        InputStream in = null;
+        FileOutputStream o=null;
+        try{
+            URL httpUrl=new URL(url);
+            connection = (HttpURLConnection) httpUrl.openConnection();
+            connection.setRequestProperty("accept", "*/*");
+            connection.setRequestProperty("Charset", "utf-8");
+            connection.setRequestProperty("connection", "Keep-Alive");
+            connection.setRequestProperty("user-agent","Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+            connection.setRequestMethod("GET");
+
+            byte[] data=new byte[1024];
+            File f=new File(path);
+            File parentDir = f.getParentFile();
+            if (!parentDir.exists()) {
+                parentDir.mkdirs();
+            }
+            if(connection.getResponseCode() == 200){
+                in = connection.getInputStream();
+                o=new FileOutputStream(path);
+                int n=0;
+                while((n=in.read(data))>0){
+                    o.write(data, 0, n);
+                    o.flush();
+                }
+            }
+            if(f.length()>2048){  //代表文件大小
+                return true;  //如果文件大于2k则返回true
+            }
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }finally{
+            try{
+                if(in != null){
+                    in.close();
+                }
+            }catch(IOException ex){
+                ex.printStackTrace();
+            }
+            try{o.close();}catch(Exception ex){}
+            try{connection.disconnect();}catch(Exception ex){}
+        }
+        return false;
+    }
+
+    /**
+     * 把GBK文件转为UTF-8
+     * 两个参数值可以为同一个路径
+     * @param srcFileName 源文件
+     * @param destFileName 目标文件 两个文件可以为一个
+     * @param fromCdoe 源文件编码(默认GBK)
+     * @param toCode 目标文件编码(默认utf-8)
+     * @throws IOException
+     */
+    private static void transferGBKToUtf8(String srcFileName, String destFileName,String fromCdoe,String toCode) throws IOException {
+        String line_separator = System.getProperty("line.separator");
+        FileInputStream fis = new FileInputStream(srcFileName);
+        StringBuffer content = new StringBuffer();
+        DataInputStream in = new DataInputStream(fis);
+        if (StringUtil.isEmpty(fromCdoe)){
+            fromCdoe = "GBK";
+        }
+        BufferedReader d = new BufferedReader(new InputStreamReader(in, fromCdoe));  //源文件的编码方式
+        String line = null;
+        while ((line = d.readLine()) != null) {
+            content.append(line + line_separator);
+        }
+        d.close();
+        in.close();
+        fis.close();
+
+        if (StringUtil.isEmpty(toCode)){
+            toCode = "utf-8";
+        }
+        Writer ow = new OutputStreamWriter(new FileOutputStream(destFileName), toCode);  //需要转换的编码方式
+        ow.write(content.toString());
+        ow.close();
     }
 }
